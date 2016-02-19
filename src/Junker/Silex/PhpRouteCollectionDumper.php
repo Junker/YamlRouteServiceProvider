2a9ff004f69862e593c55d6873e93956b63ad491
Nikita Ushakov <nikita@malefici.com>
2016-02-19 18:39:12 +0200

    CS

--- src/Junker/Silex/PhpRouteCollectionDumper.php
+++ src/Junker/Silex/PhpRouteCollectionDumper.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Junker\Silex;
 
 use Symfony\Component\Routing\RouteCollection;
 
 class PhpRouteCollectionDumper
 {
-	public static function dump(RouteCollection $collection)
-	{
-		$content = '<?php use Symfony\Component\Routing\Route; use Symfony\Component\Routing\RouteCollection; $r = new RouteCollection();' . PHP_EOL;
+    public static function dump(RouteCollection $collection)
+    {
+        $content = '<?php use Symfony\Component\Routing\Route; use Symfony\Component\Routing\RouteCollection; $r = new RouteCollection();'.PHP_EOL;
 
-		foreach ($collection->all() as $name => $route) 
-		{
-			$content .= sprintf('$z = new Route(""); $z->unserialize(\'%s\'); $r->add("%s", $z);', $route->serialize(), $name) . PHP_EOL;
-		}
+        foreach ($collection->all() as $name => $route) {
+            $content .= sprintf('$z = new Route(""); $z->unserialize(\'%s\'); $r->add("%s", $z);', $route->serialize(), $name).PHP_EOL;
+        }
 
-		$content .= 'return $r;'; 
+        $content .= 'return $r;';
 
-		return $content;
-	}
+        return $content;
+    }
 }
